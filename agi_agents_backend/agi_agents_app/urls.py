6089f53cf21dd4d113297667f87b81ee1b4a4dcd
Rammohan_Rangam <96642590+RamuRamu-12@users.noreply.github.com>
2024-09-26 14:19:09 +0530

    code updated

---
+++ agi_agents_backend/agi_agents_app/urls.py
@@ -0,0 +1,14 @@
+# agents/urls.py
+
+from django.urls import path
+from .views import agent_list, agent_detail, agent_create, modify_agent,add_email,delete
+
+urlpatterns = [
+    path('agent_list', agent_list, name='agent-list'),
+    path('agents_detail/<int:id>', agent_detail, name='agent-detail'),
+    path('agents_create', agent_create, name='agent-create'),
+    #path('admin/agent/<int:agent_id>/<str:action>/', approve_or_reject_agent, name='approve_or_reject_agent'),
+    path('agent/<int:agent_id>/modify/', modify_agent , name='modify_agent'),
+    path('agent/<int:agent_id>/delete/', delete , name='delete_agent'),
+    path('add_email', add_email, name='add_email')
+]
